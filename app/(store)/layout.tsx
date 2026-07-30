3b3cd1ad8675ef3725873ce309ea2fc8ae58068b
lucas ramon oliveira da silva <lucas.dasilva@ext.sistemafiep.org.br>
2026-07-30 17:48:24 -0300

    ajuste

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,5 +1,7 @@
 import { StoreShell } from '@/components/layout/StoreShell';
 
+export const dynamic = 'force-dynamic';
+
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
   return <StoreShell>{children}</StoreShell>;
 }
